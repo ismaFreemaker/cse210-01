ca7bf801e73041f91a8c52560c81417e58621f34
ismaFreemaker <rojasisma10@gmail.com>
2022-01-11 16:36:19 -0300

    second day of try in the tic tac toe repo in GitHub

---
+++ tic_tac_toe.py
@@ -0,0 +1,16 @@
+# hello people!!! Here is gonna be my code to play with the 
+# user a little bit when he has no one to play with...
+
+def main():
+    """here is going to be the main code """
+
+
+
+
+def table():
+    ''''''
+    print("1 | 2 | 3\
+    \n") 
+
+
+
